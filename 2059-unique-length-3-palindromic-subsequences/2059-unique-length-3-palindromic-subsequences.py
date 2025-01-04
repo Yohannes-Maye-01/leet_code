093de07f7a0a4a4056fe0e7f71e70ef9e6f967fe
Yohannes-Maye <166595409+Yohannes-Maye-01@users.noreply.github.com>
2025-01-04 00:29:41 -0800

    Time: 514 ms (63.29%), Space: 23 MB (6.15%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.py
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.py
@@ -20,11 +20,36 @@
 #                     r=r+1
 #                 l=l+1
 #         return len(windowcollict)
+
+
+# class Solution:
+#     def countPalindromicSubsequence(self, s: str) -> int:
+#         res = 0
+#         for c in set(s):  # Iterate over unique characters
+#             i, j = s.find(c), s.rfind(c)  # First and last occurrence
+#             if j > i + 1:  # Ensure there's at least one element between
+#                 res += len(set(s[i+1:j]))  # Count unique middle elements
+#         return res
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
-        res = 0
-        for c in set(s):  # Iterate over unique characters
-            i, j = s.find(c), s.rfind(c)  # First and last occurrence
-            if j > i + 1:  # Ensure there's at least one element between
-                res += len(set(s[i+1:j]))  # Count unique middle elements
-        return res
+        from collections import defaultdict
+        
+        char_indices = defaultdict(list)
+        for i, char in enumerate(s):
+            char_indices[char].append(i)
+        
+        ans = 0
+        for indices in char_indices.values():
+            start = indices[0]
+            end = indices[-1]
+
+            if end - start <= 1:
+                continue
+            
+            seen = set()
+            for i in range(start + 1, end):
+                seen.add(s[i])
+            
+            ans += len(seen)
+        
+        return ans
