8b103e6dc709137cf76b310dac1a9bac5c31c3b6
Yohannes-Maye <166595409+Yohannes-Maye-01@users.noreply.github.com>
2025-01-05 09:13:51 -0800

    Time: 3 ms (49.12%), Space: 17.6 MB (29.65%) - LeetHub

--- 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.py
+++ 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.py
@@ -1,15 +1,19 @@
-from collections import defaultdict
+# from collections import defaultdict
+# class Solution:
+#     def getSneakyNumbers(self, nums: List[int]) -> List[int]:
+#         ans=[]
+#         hash=defaultdict(list)
+#         for i,num in enumerate(nums):
+#             hash[num].append(i)
+#         for k,val in hash.items():
+#             if len(val)==2:
+#                 ans.append(k)
+#         print(ans)        
+#         return ans       
+
 class Solution:
     def getSneakyNumbers(self, nums: List[int]) -> List[int]:
-        ans=[]
-        hash=defaultdict(list)
-        for i,num in enumerate(nums):
-            hash[num].append(i)
-        for k,val in hash.items():
-            if len(val)==2:
-                ans.append(k)
-        print(ans)        
-        return ans       
 
-
+        ctr = Counter(nums)
+        return nlargest(2, ctr, key = lambda x: ctr[x])
         
